86f291094f1670b08715638fba4b41d36dc0c334
mylo19 <mylowade98@gmail.com>
2022-09-22 17:18:55 +0300

    update on ispangram based on mentoring

--- easy-functions-part1.jl
+++ easy-functions-part1.jl
@@ -45,24 +45,7 @@ Return `true` if `input` contains every alphabetic character (case insensitive).
 
 """
 function ispangram(input)
-    small = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ]
-    capital = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    length_alphabet = length(small)
-    index = []
-    for i in input
-        for j in 1:length_alphabet 
-            if i == small[j] || i == capital[j]
-                small[j] = '0'
-                capital[j] = '0'
-                push!(index, i)
-            end
-        end
-    end
-    if length(index) == length_alphabet
-        return true
-    else
-        return false
-    end
+    return issubset('a':'z', lowercase(input))
 end
 
 """
